f4fdce82147e06622826230fff2cd821340c860d
root <root@iZ235yz4wgpZ.(none)>
2014-11-26 22:51:30 +0800

    小抄列表改为动态调用，并优化htmlspecialchars功能

--- codesheet/html.php
+++ codesheet/html.php
@@ -38,7 +38,50 @@ p{
 
 </head>
 <body> 
+<?php
+    $list = array();
+    
+    //start
+    $arr = array();
+    $arr[0] = '    <a>    标签';
+    $arr[1] = '    <a>     标签定义超链接，用于从一张页面链接到另一张页面。';
+    $arr[2] = '<table><tr><th>属性</th><th>值</th><th>描述</th></tr>
+<tr><td>charset</td><td>char_encoding</td><td>HTML5 中不支持。规定被链接文档的字符集。</td></tr>
+<tr><td>coords</td><td>coordinates</td><td>HTML5 中不支持。规定链接的坐标。</td></tr>
+<tr><td>download</td><td>filename</td><td>HTML5 中的新属性。规定被下载的超链接目标。</td></tr>
+<tr><td>href</td><td>URL</td><td>规定链接指向的页面的 URL。</td></tr>
+<tr><td>hreflang</td><td>language_code</td><td>规定被链接文档的语言。</td></tr>
+<tr><td>media</td><td>media_query</td><td>HTML5 中的新属性。规定被链接文档是为何种媒介/设备优化的。</td></tr>
+<tr><td>name</td><td>section_name</td><td>HTML5 中不支持。规定锚的名称。</td></tr>
+<tr><td>rel</td><td>text</td><td>规定当前文档与被链接文档之间的关系。</td></tr>
+<tr><td>rev</td><td>text</td><td>HTML5 中不支持。规定被链接文档与当前文档之间的关系。</td></tr>
+<tr><td>shape</td><td>default<br />rect<br />circle<br />poly</td><td>HTML5 中不支持。规定链接的形状。</td></tr>
+<tr><td>target</td><td>_blank<br />_parent<br />_self<br />_top<br />framename</td><td>规定在何处打开链接文档。</td></tr>
+<tr><td>type</td><td>MIME type</td><td>HTML5 中的新属性。规定被链接文档的的 MIME 类型。</td></tr>
+</table>';
+    $arr[3] = '    <a href="http://www.baidu.com">百度</a> <!--指向 百度 的超链接-->    ';
+    $arr[4] = '浏览器支持：IE Firefox Chrome Safari Opera <br />
+    <a>    
+    元素最重要的属性是 href 属性，它指示链接的目标。 <br />
+<span class="red">提示：</span>如果不使用 href 属性，则不可以使用如下属性：download, hreflang, media, rel, target 以及 type 属性。<br />
+<span class="red">提示：</span>被链接页面通常显示在当前浏览器窗口中，除非您规定了另一个目标（target 属性）。<br />
+<span class="red">提示：</span>请使用 CSS 来设置链接的样式。<br />
+在 HTML 4.01 中，
+    <a>     
+标签可以是超链接或锚。在 HTML5 中，
+    <a>     
+    标签始终是超链接，但是如果未设置 href 属性，则只是超链接的占位符。
+HTML5 提供了一些新属性，同时不再支持一些 HTML 4.01 属性。<br />
+    <a>    
+标签支持 HTML 中的全局属性。<br />
+    <a>    
+标签支持 HTML 中的事件属性。<br />';
 
+    $list[] = $arr;
+    //end
+
+
+?>
 <div id="preloader disabled">
     <div id="status">
         <p class="center-text">
@@ -123,51 +166,35 @@ p{
             </div>
             <div class="decoration hide-if-responsive"></div>
             <!-- <a>标签 -->
+            <?php
+                foreach($list as $k => $v){
+            ?>
             <div class="container">
                 <div class="toggle-3">
-                    <a href="#" class="deploy-toggle-3"><?php echo htmlspecialchars('<a>标签');?><em class=""><strong class=""></strong></em></a>
+                    <a href="#" class="deploy-toggle-3"><?php echo preg_replace_callback('/(\s{4}.+\s{4})?/', function($matches){return htmlspecialchars($matches[0]);}, $v[0]);?><em class=""><strong class=""></strong></em></a>
                     <div class="toggle-content" style="display: none;">               
                         <p>
                             <span class="text-highlight highlight-green">定义</span>
-                            <?php echo htmlspecialchars('<a> 标签定义超链接，用于从一张页面链接到另一张页面。');?>      
+                            <?php echo preg_replace_callback('/(\s{4}.+\s{4})?/', function($matches){return htmlspecialchars($matches[0]);}, $v[1]);?>
                         </p>
                         <p>
-                            <span class="text-highlight highlight-blue">属性</span>   
+                            <span class="text-highlight highlight-blue">属性</span> 
+                            <?php echo preg_replace_callback('/(\s{4}.+\s{4})?/', function($matches){return htmlspecialchars($matches[0]);}, $v[2]);?> 
                         </p>
-                        <table >
-                            <tr><th>属性</th><th>值</th><th>描述</th></tr>
-                            <tr><td>charset</td><td>char_encoding</td><td>HTML5 中不支持。规定被链接文档的字符集。</td></tr>
-                            <tr><td>coords</td><td>coordinates</td><td>HTML5 中不支持。规定链接的坐标。</td></tr>
-                            <tr><td>download</td><td>filename</td><td>HTML5 中的新属性。规定被下载的超链接目标。</td></tr>
-                            <tr><td>href</td><td>URL</td><td>规定链接指向的页面的 URL。</td></tr>
-                            <tr><td>hreflang</td><td>language_code</td><td>规定被链接文档的语言。</td></tr>
-                            <tr><td>media</td><td>media_query</td><td>HTML5 中的新属性。规定被链接文档是为何种媒介/设备优化的。</td></tr>
-                            <tr><td>name</td><td>section_name</td><td>HTML5 中不支持。规定锚的名称。</td></tr>
-                            <tr><td>rel</td><td>text</td><td>规定当前文档与被链接文档之间的关系。</td></tr>
-                            <tr><td>rev</td><td>text</td><td>HTML5 中不支持。规定被链接文档与当前文档之间的关系。</td></tr>
-                            <tr><td>shape</td><td>default<br />rect<br />circle<br />poly</td><td>HTML5 中不支持。规定链接的形状。</td></tr>
-                            <tr><td>target</td><td>_blank<br />_parent<br />_self<br />_top<br />framename</td><td>规定在何处打开链接文档。</td></tr>
-                            <tr><td>type</td><td>MIME type</td><td>HTML5 中的新属性。规定被链接文档的的 MIME 类型。</td></tr>
-                        </table> 
                         <p>
                             <span class="text-highlight highlight-magenta">范例</span>
-                            <?php echo htmlspecialchars('<a href="http://www.baidu.com">百度</a> <!--指向 百度 的超链接-->');?>     
+                            <?php echo preg_replace_callback('/(\s{4}.+\s{4})?/', function($matches){return htmlspecialchars($matches[0]);}, $v[3]);?>    
                         </p>
                         <p>
                             <span class="text-highlight highlight-orange">注释</span>
-                            浏览器支持：IE Firefox Chrome Safari Opera <br />
-                            <?php echo htmlspecialchars('<a> 元素最重要的属性是 href 属性，它指示链接的目标。');?> <br />
-                            <span class="red">提示：</span>如果不使用 href 属性，则不可以使用如下属性：download, hreflang, media, rel, target 以及 type 属性。<br />
-                            <span class="red">提示：</span>被链接页面通常显示在当前浏览器窗口中，除非您规定了另一个目标（target 属性）。<br />
-                            <span class="red">提示：</span>请使用 CSS 来设置链接的样式。<br />
-                            <?php echo htmlspecialchars('在 HTML 4.01 中，<a> 标签可以是超链接或锚。在 HTML5 中，<a> 标签始终是超链接，但是如果未设置 href 属性，则只是超链接的占位符。
-                            HTML5 提供了一些新属性，同时不再支持一些 HTML 4.01 属性。');?><br />
-                            <?php echo htmlspecialchars('<a> 标签支持 HTML 中的全局属性。');?> <br />
-                            <?php echo htmlspecialchars('<a> 标签支持 HTML 中的事件属性。');?> <br />
+                            <?php echo preg_replace_callback('/(\s{4}.+\s{4})?/', function($matches){return htmlspecialchars($matches[0]);}, $v[4]);?>
                         </p>
                     </div>
                 </div>
             </div>
+            <?php
+                }
+            ?>
             <!-- <a>标签 -->
 
         </div>
